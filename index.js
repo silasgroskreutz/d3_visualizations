084472766ae334ecce0771a3ce1e4e05e3daed10
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-23 15:03:17 -0500

    Lesson 11 complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const canvas = d3.select('.canvas');
+
+// append the SVG container
+const svg = canvas.append('svg');
+
+// append shapes to SVG container
+svg.append('rect');
+svg.append('circle');
+svg.append('line');

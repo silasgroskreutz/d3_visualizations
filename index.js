ee15b6800e06bc9623260239feaf7d1bc49a33be
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-23 18:58:25 -0500

    Lesson 17 complete

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const svg = d3.select('svg');
 const rect = svg
   .select('rect')
   .data(data)
-  .attr('width', function(d, i, n) {
-    console.log(i);
-    console.log(n);
-    return d.width;
-  })
-  .attr('height', function(d) {
-    return d.height;
-  })
-  .attr('fill', function(d) {
-    return d.fill;
-  });
+  .attr('width', (d, i, n) => d.width)
+  .attr('height', d => d.height)
+  .attr('fill', d => d.fill);
+
+//watch what you are binding 'this' with arrow functions

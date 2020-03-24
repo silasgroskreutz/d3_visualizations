1995cccae889cb564ad5d89a0dd90596bbd749b8
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-24 10:12:08 -0500

    Lesson 28 complete

--- index.js
+++ index.js
@@ -60,8 +60,17 @@ d3.json('menu.json').then(data => {
 
   // create and call the axes, as they depend on the data calcs, they are here
   const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
 
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+
+  xAxisGroup
+    .selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+    .attr('fill', 'green');
 });

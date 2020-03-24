e2d2b70e8f464cb60f3d40edd14860747b195a03
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-23 19:22:56 -0500

    Lesson 20 complete

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const data = [
-  { width: 200, height: 100, fill: 'purple' },
-  { width: 100, height: 60, fill: 'pink' },
-  { width: 50, height: 30, fill: 'red' }
-];
-
+// select DVG container first
 const svg = d3.select('svg');
 
-// Join data to rects
-const rects = svg.selectAll('rect').data(data);
+d3.json('planets.json').then(data => {
+  const circs = svg.selectAll('circle').data(data);
 
-// Add attrs to rects already in DOM
-rects
-  .attr('width', (d, i, n) => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+  // add attrs to circs already in DOM
+  circs
+    .attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill);
 
-// append the enter selection to DOM
-rects
-  .enter()
-  .append('rect')
-  .attr('width', (d, i, n) => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+  //apend the enter selection to the DOM
+  circs
+    .enter()
+    .append('circle')
+    .attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill);
+});

25197aff577043db1befa0d57ecd5cf7feb9d296
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-24 09:38:14 -0500

    Lesson 24 complete

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const svg = d3.select('svg');
 d3.json('menu.json').then(data => {
   const y = d3
     .scaleLinear()
-    .domain([0, 1000])
+    .domain([0, d3.max(data, d => d.orders)])
     .range([0, 500]);
+
+  // const min = d3.min(data, d => d.orders);
+  // const max = d3.max(data, d => d.orders);
+  // const extend = d3.extend(data, d => d.orders);
 
   const x = d3
     .scaleBand()

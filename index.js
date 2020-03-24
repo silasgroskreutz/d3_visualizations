e4f8eeb490c7ecf52c71fd80222b59f152177a73
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-23 19:04:09 -0500

    Lesson 18 complete

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const data = [{ width: 200, height: 400, fill: 'red' }];
+const data = [
+  { width: 200, height: 100, fill: 'purple' },
+  { width: 100, height: 60, fill: 'pink' },
+  { width: 50, height: 30, fill: 'red' }
+];
 
 const svg = d3.select('svg');
 
 const rect = svg
-  .select('rect')
+  .selectAll('rect')
   .data(data)
   .attr('width', (d, i, n) => d.width)
   .attr('height', d => d.height)
   .attr('fill', d => d.fill);
-
-//watch what you are binding 'this' with arrow functions
